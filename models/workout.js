45ccbebb62903e1ad6d9d2a525dd229b2e560ced
surajgurung <microbiosuraj@gmail.com>
2020-06-29 22:19:10 -0400

    just one line of code in workout.js

--- models/workout.js
+++ models/workout.js
@@ -6,6 +6,7 @@ const workOutSchema = new Schema({
       type:{type: String, required: true},
       name: {type: String, required: true},
       duration:{type: Number, required: true},
+      distance:{type: Number, required: true},
       weight:{type: Number, required: true},
       reps: {type: Number, required: true},
       sets:{type: Number, required: true},
